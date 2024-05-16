13daac693c152405a13d195a5f2b0781502379f7
Vinicius040496 <vinicius.almeida2011@gmail.com>
2024-05-16 14:19:50 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string Msg = "";
+                double Etanol = Convert.ToDouble (txt_etanol.Text);
+                double Gasolina = Convert.ToDouble(txt_gasolina.Text);
+                if (Etanol <= (Gasolina * 0.7))
+                {
+                    Msg = "O valor do etanol está compensando";
+                }
+                else
+                {
+                    Msg = "A gasolina está compensando";
+                }
+                DisplayAlert("Calculando", Msg, "Ok");
+            }catch (Exception ex)
+            {
+                DisplayAlert("Ops",ex.Message, "Fechar");
+            }
+        }
+    }
+}
